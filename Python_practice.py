897f722083779f94ce874e9006a6d000a41dec84
sam wunderilch <swund@sams-MacBook-Pro.local>
2020-06-04 22:04:50 -0400

    updates post class

---
+++ Python_practice.py
@@ -0,0 +1,4 @@
+file_to_load = '/Users/swund/Desktop/Election_Analysis/Resources/election_results.csv'
+with open(file_to_load) as election_data:
+    for line in file:
+        print(election_data)
