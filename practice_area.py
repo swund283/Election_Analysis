d9ec76eacad14db04da82e2ba7ae35c15de8a4ae
sam wunderilch <swund@sams-MacBook-Pro.local>
2020-06-04 22:05:37 -0400

    post class updates

---
+++ practice_area.py
@@ -0,0 +1,29 @@
+# def average(numbers):
+#     length = len(numbers)
+#     total = 0.0
+#     for numbers in numbers:
+#         total += numbers
+#     return (total / length) + length
+
+# my_list_of_numbers = [1,2,3,4,5]
+# my_list_of_numbers2 = [2,3,4,5,6]
+
+# print(average(my_list_of_numbers))
+# print(average(my_list_of_numbers2))
+
+
+
+
+
+
+
+def square(numbers):
+    return pow(numbers, 2)
+
+square_me = 2
+square_me2 = 4
+square_me3 = 6
+
+print(square(square_me))
+print(square(square_me2))
+print(square(square_me3))
