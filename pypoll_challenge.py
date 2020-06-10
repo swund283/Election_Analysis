951bf2e1587b943f61e64aab0241d1d2555ee011
sam wunderilch <swund@sams-MacBook-Pro.local>
2020-06-10 13:22:46 -0400

    cleaned up text output and comments

--- pypoll_challenge.py
+++ pypoll_challenge.py
@@ -5,7 +5,7 @@ import os
 file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Initialize a total vote counter.
+# Initialize a total vote & largest county counter.
 total_votes = 0
 winning_county_count = 0
 # Candidate options and candidate votes.
@@ -27,7 +27,7 @@ with open(file_to_load) as election_data:
     for row in file_reader:
         # Add to the total vote count.
         total_votes += 1
-        # Get the candidate name from each row.
+        # Get the candidate name  & county from each row.
         candidate_name = row[2]
         county_name  = row[1]
         # If the candidate does not match any existing candidate, add the
@@ -39,23 +39,26 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
-
+        #county list
         if county_name not in county_options:
-            # Add the candidate name to the candidate list.
+            # Add the county name to the candidate list.
             county_options.append(county_name)
-            # And begin tracking that candidate's voter count.
+            # And begin tracking that county count.
             county_votes[county_name] = 0
-          # Add a vote to that candidate's count.
+          # Add a vote to that county's count.
         county_votes[county_name] += 1
 
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
-    # Print the final vote count to the terminal.
+    # Print the final vote count to the terminal.  added some padding here for the output whitespace to keep it out of the next for loop
     election_results = (
         f"\nElection Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n")
+        f"-------------------------\n"
+        f"\n"
+        f"County Votes:\n"
+        f"\n")
     print(election_results, end="")
     # Save the final vote count to the text file.
     txt_file.write(election_results)
@@ -65,7 +68,7 @@ with open(file_to_save, "w") as txt_file:
         county_vote = county_votes[county]
         county_vote_percentage = float(county_vote) / float(total_votes) * 100
         county_results = (
-            f"{county}: {county_vote_percentage:.1f}% ({county_vote:,})\n")
+            f"{county}: {county_vote_percentage:.1f}% ({county_vote:,})\n\n")
          # Print each county's vote count and percentage to the terminal.
         print(county_results)
         #  Save the county results to our text file.
